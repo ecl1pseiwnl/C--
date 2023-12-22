0f04f55220a82f2a9ece6cfa4ec8c9f5d016c4aa
ecl1pse iwnl <hoanglocchu7@gmail.con>
2023-12-22 16:52:51 +0700

    22/12/23

--- XOAYOC2_HVT/XOAYOC2_HVT/XOAYOC2.cpp
+++ XOAYOC2_HVT/XOAYOC2_HVT/XOAYOC2.cpp
@@ -17,5 +17,30 @@ int main() {
 		}
 	}
 	sort(v.begin(), v.end());
-
+	int k = 0, p = 0, col = n, row = m;
+	while (k + 1 <= m * n) {
+		for (int i = p; i < col; i++) {
+			a[p][i] = v[k++];
+		}
+		for (int j = p + 1; j < row; j++) {
+			a[j][col - 1] = v[k++];
+		}
+		if (p != row - 1) {
+			for (int i = col - 2; i >= p; i--) {
+				a[row - 1][i] = v[k++];
+			}
+		}
+		if (p != col - 1) {
+			for (int i = row - 2; i > p; i--) {
+				a[i][p] = v[k++];
+			}
+		}
+		p++; row--; col--;
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j] << ' ';
+		}
+		cout << endl;
+	}
 }
