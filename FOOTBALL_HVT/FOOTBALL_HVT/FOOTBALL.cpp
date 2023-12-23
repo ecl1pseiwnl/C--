38b42753ed7169372efbed64b406c76679f8a41d
ecl1pse iwnl <hoanglocchu7@gmail.con>
2023-12-23 17:17:01 +0700

    23/12/23

--- FOOTBALL_HVT/FOOTBALL_HVT/FOOTBALL.cpp
+++ FOOTBALL_HVT/FOOTBALL_HVT/FOOTBALL.cpp
@@ -1,29 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int a[102][102];
 
-bool dgcheochinh(int a[102][102], int n) {
-	for (int i = 0; i < n; i++) {
-		if (a[i][i] != 0) {
-			return false;
-		}
-	}
-	return true;
-}
-
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	int n;
 	cin >> n;
+	vector<int> v;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> a[i][j];
 		}
 	}
-	if (dgcheochinh(a, n) == true) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
 
+	int highestScore = 0;
+	for (int i = 0; i < n; i++) {
+		int TotalScore = 0;
+		for (int j = 0; j < n; j++) {
+			TotalScore += a[i][j];
+		}
+		v.push_back(TotalScore);
+	}
+	for (int i = 0; i < n; i++) {
+		if (highestScore < v[i]) {
+			highestScore = v[i];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		int sum = 0;
+		for (int j = 0; j < n; j++) {
+			sum += a[i][j];
+			if (sum == highestScore) {
+				cout << i + 1 << ' ';
 			}
+		}
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		int W = 0, L = 0;
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] == 3 && a[i][j] != a[i][i]) {
+				W++;
+			}
+			else if (a[i][j] == 0 || a[i][j] == 1) {
+				L++;
+			}
+		}
+		if (W > L - 1) {
+			cout << i + 1 << ' ';
+		}
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		int L = 0;
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] == 0) {
+				L++;
+			}
+		}
+		if (L - 1 == 0) {
+			cout << i + 1 << ' ';
 		}
 	}
 }
