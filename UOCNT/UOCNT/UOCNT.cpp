f65e3d3f3b7e36c31f73fca89d5370768a6af8f7
ecl1pse iwnl <hoanglocchu7@gmail.con>
2024-01-07 21:53:25 +0700

    7/1/24

--- UOCNT/UOCNT/UOCNT.cpp
+++ UOCNT/UOCNT/UOCNT.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+// cach 1: O(logn)
+const ll N = 1e14;
+ll a[N];
+
+void sang() {
+    for (ll i = 2; i <= N; i++) {
+        a[i] = 1;
+    }
+    a[0] = a[1] = 0;
+    for (ll i = 2; i <= sqrt(N); i++) {
+        if (a[i] == 1) {
+            for (ll j = i * i; j < N; j += i) {
+                a[j] = 0;
+            }
+        }
+    }
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ll n, s = 0;
+    cin >> n;
+    sang();
+    for (ll i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0 && a[i] == 1) {
+            s += i;
+        }
+    }
+    cout << s;
+}
+// Cach 2
+int main2() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     long long n;
     cin >> n;
     int cnt;
@@ -18,8 +52,8 @@ int main() {
     }
     cout << sum << endl;
 }
-// cach 2:
 
+// cach 3:
 int snt(int N) {
     if (N < 2) return 0;
     for (int i = 2; i * i <= N; i++) {
