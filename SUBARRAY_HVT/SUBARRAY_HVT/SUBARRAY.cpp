4925b74546644e9fec900f16de5975c098dd9b50
ecl1pse iwnl <hoanglocchu7@gmail.con>
2024-01-19 18:16:38 +0700

    19/1/24

--- SUBARRAY_HVT/SUBARRAY_HVT/SUBARRAY.cpp
+++ SUBARRAY_HVT/SUBARRAY_HVT/SUBARRAY.cpp
@@ -3,11 +3,32 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
-#define mp make_pair;
+typedef set<int> se;
+typedef map<int, int> mp;
+#define endl "\n"
+#define mp make_pair
 #define pb push_back
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k, cnt = 0;
+	cin >> n >> k;
+	vi v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	vi dp(n, 1);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (v[i] + v[j] >= k) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+	for (auto x : dp) {
+		cout << x << ' ';
+	}
+	cout << endl;
+	cout << *min_element(dp.begin(), dp.end());
 }
