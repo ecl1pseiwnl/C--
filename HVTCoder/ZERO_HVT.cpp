9d0c7c269c759b2bfbfc5f2e3dda136290ccec8c
ecl1pse iwnl <hoanglocchu7@gmail.con>
2024-04-14 20:56:59 +0700

    14/4/24

--- HVTCoder/ZERO_HVT.cpp
+++ HVTCoder/ZERO_HVT.cpp
@@ -3,13 +3,38 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
-typedef set<int> se;
+typedef set<int> sii;
 typedef map<int, int> mii;
 #define endl "\n";
 #define mp make_pair
 #define pb push_back
 
+const int N = 10005;
+int n, d[N], ans, a[N];
+
+void dfs(int u) {
+    d[n] = 1;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            int j = n / i;
+            int m = (i - 1) * (j + 1);
+            if (!d[m]) {
+                a[m] = 1, ans++;
+                dfs(m);
+            }
+        }
+    }
+}
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    dfs(n);
+    a[0] = 1;
+    cout << ans + 1 << endl;
+    for (int i = 0; i < N; i++) {
+        if (a[i]) cout << i << " ";
+    }
+    return 0;
 }
