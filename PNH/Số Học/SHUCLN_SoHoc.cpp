06dbf64004f0de2916025082091e4a66bc72a80c
ecl1pse iwnl <hoanglocchu7@gmail.con>
2024-04-17 13:02:26 +0700

    17/4/24

---
+++ PNH/Số Học/SHUCLN_SoHoc.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> si;
+typedef map<int, int> mii;
+#define MOD 10000000007
+#define endl "\n";
+#define mp make_pair
+#define pb push_backa
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        cout << __gcd(a, b) << endl;
+    }
+    return 0;
+}
