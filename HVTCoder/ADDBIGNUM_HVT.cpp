7c415a5827434d2b62019c2d505eaa788c48cf4b
ecl1pse iwnl <hoanglocchu7@gmail.con>
2024-04-18 21:35:37 +0700

    18/4/24

--- HVTCoder/ADDBIGNUM_HVT.cpp
+++ HVTCoder/ADDBIGNUM_HVT.cpp
@@ -1,10 +1,36 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef map<int, int> mii;
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+#define MOD 10000007
+
+string a, b;
+string csl(string a, string b) {
+    int du = 0;
+    int mid = 0;
+    string res = "";
+    a.insert(0, max(0, (int)(b.length() - a.length())), '0');
+    b.insert(0, max(0, (int)(a.length() - b.length())), '0');
+    for (int i = a.length() - 1; i >= 0; --i)
+    {
+        mid = ((int)a[i] - 48) + ((int)b[i] - 48) + du;
+        du = mid / 10;
+        res = (char)(mid % 10 + 48) + res;
+    }
+    if (du > 0) res = "1" + res;
+    return res;
+}
 
 int main() {
-	ll(a); ll(b);
-	cin >> a >> b;
-	cout << a + b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string a, b;
+    cin >> a >> b;
+    cout << csl(a, b);
+    return 0;
 }
