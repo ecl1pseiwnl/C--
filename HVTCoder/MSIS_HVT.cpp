05c80cb41235e37a936eab745885d66726b9241a
ecl1pse iwnl <hoanglocchu7@gmail.com>
2024-05-31 21:59:47 +0700

    31/5/24

---
+++ HVTCoder/MSIS_HVT.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef map<int,int> mii;
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+#define MOD 10000007
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int n, ans = INT_MIN;
+    cin >> n;
+    vector<int> a(n+1), dp(n+1,1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) {
+        int tmp = a[i];
+        for (int j = min(i-10, 1); j < i; j++) {
+            if (a[j] < a[i]) {
+                tmp += a[j];
+            }
+        }
+        ans = max(tmp,ans);
+    }
+    cout << ans;
+    return 0;
+}
