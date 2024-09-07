7dcaaf801332b45374994f8085ace6028944d2fb
ecl1pse iwnl <hoanglocchu7@gmail.com>
2024-09-07 22:14:02 +0700

    7/9/24

---
+++ Data Structure And Algorithms/Graphs/Advanced (HSG,THT,...)/FloydWarshallAlgorithm_Graphs.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef map<int,int> mii;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+typedef vector<mii> viii;
+ll __lcm(ll x, ll y) { return 1ll*(x / __gcd(x,y))*y;}
+#define ms(a,n) memset(a, n,sizeof(a))
+#define all(x) (x).begin(),(x).end()
+#define ins insert
+#define sz(x) (int)(x.size())
+#define name "TASK"
+#define fi first
+#define sec second
+#define mp make_pair
+#define ep emplace_back
+#define pb push_back
+#define endl "\n"
+const int MOD = 1e9+7;
+const int32_t IINF = 0x3f3f3f3f;
+const int64_t LLINF = 0x3f3f3f3f3f3f3f3f;
+
+const int N = 250007, M = 500;
+ll n,m, dist[M+7][M+7];
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    if (fopen(name ".inp", "r")) {
+        freopen(name ".inp","r",stdin);
+        freopen(name ".out","w",stdout);
+    }
+    cin >> n >> m;
+    for (int i = 1; i <= n+1; i++) {
+        for (int j = 1; j <= n+1; j++) {
+            dist[i][j] = LLINF;
+        }
+        dist[i][i] = 0;
+    }
+    for (ll i = 0,x,y,z; i < m; i++) {
+        cin >> x >> y >> z;
+        dist[x][y] = min(dist[x][y],z);
+        dist[y][x] = min(dist[y][x],z);
+    }
+    for (int k = 1; k <= n+1; k++) {
+        for (int i = 1; i <= n+1; i++) {
+            for (int j = 1; j <= n+1; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+            }
+        }
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            if (dist[i][j] == LLINF) {
+                cout << "INF ";
+            } else {
+                cout << dist[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
