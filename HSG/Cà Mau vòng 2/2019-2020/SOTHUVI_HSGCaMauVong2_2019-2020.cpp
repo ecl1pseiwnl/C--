cc7e80f951710468d05d9f849896f1e27d373c9e
ecl1pse iwnl <hoanglocchu7@gmail.com>
2024-10-09 17:04:57 +0700

    9/10/24

---
+++ HSG/Cà Mau vòng 2/2019-2020/SOTHUVI_HSGCaMauVong2_2019-2020.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef map<int,int> mii;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+typedef vector<mii> viii;
+ll __lcm(ll x, ll y) { return 1ll*(x / __gcd(x,y))*y;}
+#define ms(a,n) memset(a, n,sizeof(a))
+#define all(x) (x).begin(),(x).end()
+#define ins insert
+#define uniquev(v) sort(all(v)), (v).resize(unique(all(v)) - (v).begin())
+#define sz(x) (int)(x.size())
+#define name "TASK"
+#define fi first
+#define sec second
+#define mp make_pair
+#define ep emplace_back
+#define pb push_back
+#define endl "\n"
+const int MOD = 1e9+7;
+const int32_t IINF = 0x3f3f3f3f;
+const int64_t LLINF = 0x3f3f3f3f3f3f3f3f;
+
+int n,k,cnt;
+bool sokhop(const string& s) {
+    if (s.size() == 0 || s.size() == 1) return false;
+    int l = 0, r = k;
+    while (r <= s.size()) {
+        string t = s.substr(l,r);
+        if (t.size() != k) return false;
+        sort(all(t));
+        t.resize(unique(all(t)) - t.begin());
+        if (t.size() == 1) {
+            return true;
+        } else {
+            l++;
+        }
+    }
+    return false;
+}
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        string s;
+        cin >> s;
+        if (sokhop(s)) cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
